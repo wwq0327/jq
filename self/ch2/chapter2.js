69d16fce4b78449c8a844b1ac1685d87723bac60
wwq0327 <wwq0327@gmail.com>
2011-10-22 21:24:00 +0800

    选择器

--- self/ch2/chapter2.js
+++ self/ch2/chapter2.js
@@ -1,10 +1,21 @@
 $(document).ready(function() {
-	$('#selected-plays > li').addClass('horizontal');
-	$('#selected-plays li:not(.horizontal)').addClass('sub-level');
+	$('#selected-plays > li').addClass('horizontal'); // 顶级的li标签
+	$('#selected-plays li:not(.horizontal)').addClass('sub-level'); // 没有包含.horizontal类的li标签。
 });
 
 $(document).ready(function() {
-	$('a[href^=mailto:]').addClass('mailto'); // 
-	$('a[href$=.pdf]').addClass('pdflink');
-	$('a[href^=http][href*=henry]').addClass('henrylink');
+	$('a[href^=mailto:]').addClass('mailto');  // ^，开头匹配
+	$('a[href$=.pdf]').addClass('pdflink'); // $，结尾匹配
+	$('a[href^=http][href*=henry]').addClass('henrylink'); //组合， *表示任意位置匹配。
+});
+
+/*
+$(document).ready(function() {
+    $('tr:odd').addClass('alt'); // odd偶数行开始，0为偶数，表格第一行为偶数行。
+});
+*/
+
+$(document).ready(function() {
+    $('tr:nth-child(even)').addClass('alt'); // nth-child第一行，even奇数行。
+    $('td:contains(Henry)').addClass('highlight');
 });
